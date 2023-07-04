44cf6a749c77c992c894622ea52b19c7282f8fb6
ZKDeGitHub <ke15290172726@gmail.com>
2023-07-04 23:52:16 +0800

    7.4-2

--- src/main/java/com/edu/day0704/exercise6/aspect/CustomAspect.java
+++ src/main/java/com/edu/day0704/exercise6/aspect/CustomAspect.java
@@ -1,18 +1,11 @@
 package com.edu.day0704.exercise6.aspect;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.stereotype.Component;
-
-//声明spring容器管理
-@Component
-//声明是切面类
-@Aspect
+/**
+ * @author: zk
+ * @className: Server
+ * @description: ToDo
+ * @date: 2023/07/04/23:02
+ * @since: 11
+ */
 public class CustomAspect {
-
-    // 替换xml中的切入点配置
-    @Pointcut
-    public void pc(){
-
-    }
 }
--- src/main/java/com/edu/day0704/exercise6/config/SpringConfig.java
+++ src/main/java/com/edu/day0704/exercise6/config/SpringConfig.java
@@ -1,11 +1,17 @@
 package com.edu.day0704.exercise6.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
 /**
  * 替换xml文件配置
  */
 //替换xml文件头
 @Configuration
-
+//开启包扫描，相当于xml中 <context:component-scan base-package="com.edu"></context:component-scan>
+@ComponentScan("com.edu.day0704.exercise6")
+//开启AOP，相当于xml中 <aop:aspectj-autoproxy></aop:aspectj-autoproxy>
+@EnableAspectJAutoProxy
 public class SpringConfig {
 }
